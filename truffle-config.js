0b4e322525dd6a5cf2d673d157520c53c3c17d7e
hhio618 <hhio618@gmail.com>
2021-01-31 18:39:18 +0000

    add example env file

--- truffle-config.js
+++ truffle-config.js
@@ -41,7 +41,7 @@ module.exports = {
         // Useful for deploying to a public network.
         // NB: It's important to wrap the provider as a function.
         rinkeby: {
-            provider: () => new HDWalletProvider(process.env.DEV_MNEMONIC, "https://rinkeby.infura.io/v3/" + infuraProjectId),
+            provider: () => new HDWalletProvider(process.env.DEV_PRIVATE_KEY, "https://rinkeby.infura.io/v3/" + infuraProjectId),
             network_id: 4,       // Ropsten's id
             gas: 5500000,        // Ropsten has a lower block limit than mainnet
             confirmations: 2,    // # of confs to wait between deployments. (default: 0)
